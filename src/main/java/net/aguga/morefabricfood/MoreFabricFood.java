61119c3871ad86dcb7a52607758396cbb2105fcb
Aguga2201 <simon.meyer@mein.gmx>
2022-05-09 20:38:55 +0200

    Test Item, Test Block

--- src/main/java/net/aguga/morefabricfood/MoreFabricFood.java
+++ src/main/java/net/aguga/morefabricfood/MoreFabricFood.java
@@ -1,16 +1,23 @@
 package net.aguga.morefabricfood;
 
+import net.aguga.morefabricfood.block.ModBlocks;
+import net.aguga.morefabricfood.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MoreFabricFood implements ModInitializer
 {
-    public static final Logger LOGGER = LoggerFactory.getLogger("morefabricfood");
+    public static final String MOD_ID = "morefabricfood";
+    public static final String NAME = "MoreFabricFood";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize()
     {
-        LOGGER.info("Hello Fabric world!");
+        LOGGER.info(NAME + " succesfully loaded!");
+
+        ModItems.registerModItems();
+        ModBlocks.registerModBlocks();
     }
 }
---
+++ src/main/java/net/aguga/morefabricfood/item/ModItems.java
@@ -0,0 +1,27 @@
+package net.aguga.morefabricfood.item;
+
+import net.aguga.morefabricfood.MoreFabricFood;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+import java.sql.Struct;
+
+public class ModItems
+{
+    public static final Item IGNORE = registerItem("ignore",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    private static Item registerItem(String name, Item item)
+    {
+        return Registry.register(Registry.ITEM, new Identifier(MoreFabricFood.MOD_ID, name), item);
+    }
+
+    public static void registerModItems()
+    {
+        MoreFabricFood.LOGGER.info("Regestering Mod Items for " + MoreFabricFood.NAME + ".");
+
+    }
+}
