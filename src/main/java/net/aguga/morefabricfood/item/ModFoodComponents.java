f5f847e53a8ea568591af4bbc067e9a99646de22
Aguga2201 <simon.meyer@mein.gmx>
2022-05-11 15:29:40 +0200

    Added Raspberry and Strawberry Jam Bread

--- src/main/java/net/aguga/morefabricfood/item/ModFoodComponents.java
+++ src/main/java/net/aguga/morefabricfood/item/ModFoodComponents.java
@@ -6,4 +6,7 @@ public class ModFoodComponents
 {
     public static final FoodComponent STRAWBERRY = new FoodComponent.Builder().hunger(2).saturationModifier(0.2f).build();
     public static final FoodComponent RASPBERRY = new FoodComponent.Builder().hunger(2).saturationModifier(0.2f).build();
+
+    public static final FoodComponent STRAWBERRY_JAM_BREAD = new FoodComponent.Builder().hunger(8).saturationModifier(0.2f).build();
+    public static final FoodComponent RASPBERRY_JAM_BREAD = new FoodComponent.Builder().hunger(8).saturationModifier(0.2f).build();
 }
--- src/main/java/net/aguga/morefabricfood/item/ModItems.java
+++ src/main/java/net/aguga/morefabricfood/item/ModItems.java
@@ -11,17 +11,25 @@ import java.sql.Struct;
 
 public class ModItems
 {
+    //Strawberrys
     public static final Item STRAWBERRY = registerItem("strawberry",
             new Item(new FabricItemSettings().group(ModItemGroup.MORE_FABRIC_FOOD).food(ModFoodComponents.STRAWBERRY)));
 
     public static final Item STRAWBERRY_JAM = registerItem("strawberry_jam",
             new Item(new FabricItemSettings().group(ModItemGroup.MORE_FABRIC_FOOD)));
 
+    public static final Item STRAWBERRY_JAM_BREAD = registerItem("strawberry_jam_bread",
+            new Item(new FabricItemSettings().group(ModItemGroup.MORE_FABRIC_FOOD).food(ModFoodComponents.STRAWBERRY_JAM_BREAD)));
+
+    //Raspberrys
     public static final Item RASPBERRY = registerItem("raspberry",
             new Item(new FabricItemSettings().group(ModItemGroup.MORE_FABRIC_FOOD).food(ModFoodComponents.RASPBERRY)));
 
     public static final Item RASPBERRY_JAM = registerItem("raspberry_jam",
             new Item(new FabricItemSettings().group(ModItemGroup.MORE_FABRIC_FOOD)));
+
+    public static final Item RASPBERRY__JAM_BREAD = registerItem("raspberry_jam_bread",
+            new Item(new FabricItemSettings().group(ModItemGroup.MORE_FABRIC_FOOD).food(ModFoodComponents.RASPBERRY_JAM_BREAD)));
 
     private static Item registerItem(String name, Item item)
     {
